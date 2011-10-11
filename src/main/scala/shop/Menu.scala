b55bbc38d9f02327c247ccdcd53eb3e1ade49887
Jan Vermeir <jvermeir@xebia.com>
2011-10-11 13:17:18 +0200

    melding

--- src/main/scala/shop/Menu.scala
+++ src/main/scala/shop/Menu.scala
@@ -3,18 +3,45 @@ package shop
 import scala.annotation.tailrec
 import org.apache.commons.io.FileUtils
 import java.io.File
+import org.joda.time.DateTime
+import org.joda.time.format._
 
-class Menu(listOfRecipes: List[(String, String)], cookbook: CookBook) {
+class Menu(listOfRecipes: List[(String, String)], cookbook: CookBook, dateOfSaturday: DateTime) {
   val recipes = for (recipe <- listOfRecipes) yield cookbook.findRecipe(recipe._2)
-  val ingredients = for (recipe <- recipes) yield recipe.ingredients
+  var dayNumber:Int = 0
+  var theDate = dateOfSaturday
+  val ingredients = 
+    for {recipe <- recipes} 
+    yield {theDate = theDate.plusDays(1); getIngredientsWithDateAdded(recipe, theDate)}
   val sortedListOfIngredients = ingredients.flatten.sort(_ < _)
-
+  
   def printMenu: String = {
     var menu: String = ""
     for (recipe <- listOfRecipes) {
       menu = menu + recipe._1 + ":" + recipe._2 + "\n"
     }
     menu
+  }
+
+  def getIngredientsWithDateAdded(recipe: Recipe, date:DateTime): List[IngredientForMenu] = {
+    def recursiveAdd(ingredients: List[Ingredient]): List[IngredientForMenu] = {
+      ingredients match {
+        case Nil => List()
+        case head :: tail => new IngredientForMenu(head, date) :: recursiveAdd(tail)
+      }
+    }
+    recursiveAdd(recipe.ingredients)
+  }
+
+  def getIngredientsWithDateAdded(recipe: Recipe, dateOfSaturday: DateTime, dayNumber: Int): List[IngredientForMenu] = {
+    val date:DateTime = dateOfSaturday.plusDays(dayNumber)
+    def recursiveAdd(ingredients: List[Ingredient]): List[IngredientForMenu] = {
+      ingredients match {
+        case Nil => List()
+        case head :: tail => new IngredientForMenu(head, date) :: recursiveAdd(tail)
+      }
+    }
+    recursiveAdd(recipe.ingredients)
   }
 
   def printShoppinglistForUseWhileShopping: String =
@@ -38,11 +65,14 @@ class Menu(listOfRecipes: List[(String, String)], cookbook: CookBook) {
     }
     list
   }
+
 }
 
 object Menu {
   def apply(menuAsString: String, cookbook: CookBook): Menu = {
     val menuAsListOfStrings = List.fromArray(menuAsString.split("\n"))
+    val dateOfSaturday = parseDateForSaturday(menuAsListOfStrings(0).split(":")(1).trim)
+
     @tailrec def recursiveAdd(menuAsListOfStrings: List[String], menu: List[(String, String)]): List[(String, String)] = {
       menuAsListOfStrings match {
         case Nil => menu.reverse
@@ -51,15 +81,21 @@ object Menu {
           else recursiveAdd(menuAsListOfStrings.drop(1), menu)
       }
     }
-    new Menu(recursiveAdd(menuAsListOfStrings, List[(String, String)]()), cookbook: CookBook)
+    new Menu(recursiveAdd(menuAsListOfStrings.drop(1), List[(String, String)]()), cookbook: CookBook, dateOfSaturday)
   }
 
-  def createMenuLineFromTextLine(textLine:String):Tuple2[String, String] = {
-    (textLine.split(":")(0).trim,textLine.split(":")(1).trim)
+  def createMenuLineFromTextLine(textLine: String): Tuple2[String, String] = {
+    (textLine.split(":")(0).trim, textLine.split(":")(1).trim)
   }
-  
+
   def readFromFile(fileName: String, cookBook: CookBook): Menu = {
     val menuAsText = FileUtils.readFileToString(new File(fileName))
     apply(menuAsText, cookBook)
   }
+
+  def parseDateForSaturday(saturday: String): DateTime = {
+    val fmt = DateTimeFormat forPattern "ddMMyyyy"
+    fmt parseDateTime saturday
+  }
 }
+
