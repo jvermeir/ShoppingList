40a25f30185be963dcccc9d49fc3f1b9f61e70dd
Jan Vermeir <jvermeir@xebia.com>
2011-10-14 22:13:41 +0200

    Refactored dummy ingredient

--- src/main/scala/shop/Ingredient.scala
+++ src/main/scala/shop/Ingredient.scala
@@ -25,7 +25,7 @@ object Ingredient {
     val ingredient = ingredientLine.split(":")
     ingredient.length match {
       case 2 => new Ingredient(ingredient(0).trim(), ingredient(1).trim())
-      case _ => new Ingredient("dummy", "dummy")
+      case _ => null //new Ingredient("dummy", "dummy")
     }
   }
 }
--- src/main/scala/shop/Recipe.scala
+++ src/main/scala/shop/Recipe.scala
@@ -16,16 +16,13 @@ object Recipe {
    * The lines following the name line refer to ingredients. Each ingredient is formatted as:
    *    <category>:<name>
    * where <category> refers to some label that makes sense to locate the ingredient in a shop and name is the name of the ingredient.
-   * 
    */
   def apply(receptAsText: String): Recipe = {
     val receptAsLinesOfText: List[String] = List.fromArray(receptAsText.split("\n"))
     apply(receptAsLinesOfText)
   }
   
-  /*
-   * Create a recipe from a list of strings. See comment for apply(String).
-   */
+  /* Create a recipe from a list of strings. See comment for apply(String). */
   def apply(recipeAsListOfLines: List[String]): Recipe = {
     val name = recipeAsListOfLines(0).split(":")(1)
     val ingredients = for (ingredient <- recipeAsListOfLines) yield (Ingredient(ingredient))
@@ -33,15 +30,14 @@ object Recipe {
   }
 
   def isValidIngredientLine(ingredient: Ingredient): Boolean = {
-    thisIsNotADummy(ingredient) && thisIsNotAName(ingredient)
+    ingredientIsNotEmpty(ingredient) && thisIsNotAName(ingredient)
   }
 
   def thisIsNotAName(ingredient: Ingredient): Boolean = {
     ingredient.category.length > 0 && !ingredient.category.equals("naam")
   }
 
-  // TODO: do we still need this? Dummy sounds a bit dumb...
-  def thisIsNotADummy(ingredient: Ingredient): Boolean = {
-    ingredient.name.length > 0 && !ingredient.name.equals("dummy")
+  def ingredientIsNotEmpty(ingredient: Ingredient): Boolean = {
+    ingredient != null && ingredient.name.length > 0 
   }
 }
