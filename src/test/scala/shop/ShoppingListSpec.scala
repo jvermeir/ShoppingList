12bbbe329e0e67a2988d0c64e0990c11785c6f07
Jan Vermeir <jvermeir@xebia.com>
2011-10-24 17:44:22 +0200

    Moved test from Todo to ShoppingListSpec

--- src/test/scala/shop/ShoppingListSpec.scala
+++ src/test/scala/shop/ShoppingListSpec.scala
@@ -189,6 +189,32 @@ olie:augurken
       zilveruitjes"""
       expectedShoppingList must be === shoppingList.printShoppinglistForUseWhileShopping
     }
+
+    scenario("A list of extra's (not related to a recipe) may be added to a menu") {
+      given("a menu with one recipe and a list of extra's")
+      val menuAsString = """Zaterdag valt op:08102011
+      	zaterdag:dish1
+
+        extra
+      	meel:meel
+      	zeep:vaatwasmiddel
+      """
+      val cookBook = """naam:dish1
+groente:witlof
+"""
+      when("a shoppinglist is generated")
+      val menuAndList = ShoppingList.split(menuAsString)
+      val menu = Menu(menuAndList._1, CookBook(cookBook))
+      val extras: List[Ingredient] = Ingredient.readFromText(menuAndList._2)
+      val shoppingList = new ShoppingList(menu, extras)
+      then("the list contains witlof and the two extra's")
+      val expectedShoppingList = """zaterdag:dish1
+
+groente:witlof(08-10)
+meel:meel
+zeep:vaatwasmiddel"""
+      expectedShoppingList must be === shoppingList.printShoppinglistForUseWhileShopping
+    }
   }
 
   val cookBookAsText = """naam:Witlof met kip
