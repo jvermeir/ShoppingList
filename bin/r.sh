82c0af984b577e147571cb5aeae20810d217776a
Jan Vermeir <jvermeir@hotmail.com>
2020-06-26 20:23:45 +0200

    introduce versioned cookbook file

--- bin/r.sh
+++ bin/r.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./bin/runShop.sh ./data/cookbook.txt ./menus/$1.txt > $1.txt && open $1.txt
+./bin/runShop.sh ./data/cookbook_v2.txt ./menus/$1.txt > $1.txt && open $1.txt
 
 cp $1.txt ~/Dropbox/transfer
