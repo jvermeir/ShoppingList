d88079fd8f42fb554b745738ec255cd3af5fc75a
Jan Vermeir <jvermeir@xebia.com>
2021-01-06 17:25:01 +0100

    improved scripts

--- bin/r.sh
+++ bin/r.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-BIN=$(dirname "$(readlink "${BASH_SOURCE[0]}")")
+if [ -f ${BASH_SOURCE[0]} ];
+then
+  BIN=$(dirname "$(readlink "${BASH_SOURCE[0]}")")
+else
+  BIN=$(dirname ${BASH_SOURCE[0]})
+fi
+
 DATA=${BIN}/../../ShoppingListData/
-echo ${BIN}
-echo ${DATA}
 
 ${BIN}/runShop.sh ${DATA}/cookbook_v2.txt ${DATA}/menus/$1.txt ${DATA}/categoryDatabase_v2.csv >$1.txt && open $1.txt
 
--- bin/runShop.sh
+++ bin/runShop.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ -L ${BASH_SOURCE[0]} ];
+then
+  BIN=$(dirname "$(readlink "${BASH_SOURCE[0]}")")
+else
+  BIN=$(dirname ${BASH_SOURCE[0]})
+fi
+
 if test $# -lt 3
 then
 	echo "Usage: runShop.sh <cookbook> <menu> <categoryDatabase>"
 	exit 0
 fi
-java -cp bin/shoppinglist-assembly-1.7.jar shop.Main $1 $2 $3
+
+java -cp ${BIN}/shoppinglist-assembly-1.7.jar shop.Main $1 $2 $3
