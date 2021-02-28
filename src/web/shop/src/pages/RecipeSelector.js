68ed04ae02ee2c147e1cdbf1e198e4899c932fb8
Jan Vermeir <jvermeir@hotmail.com>
2021-02-28 19:25:54 +0100

    lousy work around for empty recipe field

--- src/web/shop/src/pages/RecipeSelector.js
+++ src/web/shop/src/pages/RecipeSelector.js
@@ -6,9 +6,6 @@ export function RecipeSelector({recipeList, theMenuItem, updateMenu}) {
     const [recipes] = useState(recipeList);
 
     function renderMenuItem(menuItem, val) {
-        // if (val === "" || menuItem.toLowerCase().indexOf(val.toLowerCase()) !== -1) {
-        //     console.log({"event": "renderMenuItem, found match",menuItem, val});
-        // }
         return (
             val === "" || menuItem.toLowerCase().indexOf(val.toLowerCase()) !== -1
         );
@@ -34,13 +31,19 @@ export function RecipeSelector({recipeList, theMenuItem, updateMenu}) {
                         </div>
                     }
                     onChange={(event, val) => {
+                        // TODO: this event should not cause a rest call, so updateMenu should not be called
+                        // if the field is emptied, it gets a default of -, this value is set automatically and should
+                        // therefore trigger updateMenu(). this seems a bit complicated...
+                        // now you can't empty the field because it will get a value of -
                         const finalRecipeName = val === "" ? "-" : val;
                         setMenuItem(finalRecipeName);
-                        console.log({"event":"onChange", "val":finalRecipeName});
-                        updateMenu(val, theMenuItem);
+                        console.log({"event": "onChange", "val": finalRecipeName});
+                        if (finalRecipeName === "-") {
+                            updateMenu(finalRecipeName, theMenuItem);
+                        }
                     }}
                     onSelect={(val) => {
-                        console.log({"event":"onSelect", "val":val});
+                        console.log({"event": "onSelect", "val": val});
                         setMenuItem(val);
                         updateMenu(val, theMenuItem);
                     }}
