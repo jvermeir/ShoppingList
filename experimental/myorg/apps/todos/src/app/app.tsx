b16dabb8835129f404c8bda07de5fc0d055a5224
Jan Vermeir <jvermeir@hotmail.com>
2022-04-13 21:36:47 +0200

    nx

---
+++ experimental/myorg/apps/todos/src/app/app.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { Todo } from '@myorg/data';
+import { Todos } from '@myorg/ui';
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then((_) => _.json())
+      .then(setTodos);
+  }, []);
+
+  function addTodo() {
+    fetch('/api/addTodo', {
+      method: 'POST',
+      body: '',
+    })
+      .then((_) => _.json())
+      .then((newTodo) => {
+        setTodos([...todos, newTodo]);
+      });
+  }
+
+  return (
+    <>
+      <h1>Todos</h1>
+      <Todos todos={todos} />
+      <button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </button>
+    </>
+  );
+};
+
+export default App;
